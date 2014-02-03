79e20517d9ae34a89eae64163a6c652079e05200
danthomas <danthomas000@gmail.com>
2014-02-03 11:40:17 +0000

    commit

---
+++ FitnesseUtils/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FitnesseUtils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Settings settings = new Settings(args);
+
+            if (settings.Load())
+            {
+                if (settings.Clear)
+                {
+                    Clear(settings.LocalDirectory);
+                }
+
+                if (settings.Get)
+                {
+                    Get(settings);
+                }
+            }
+            else
+            {
+                Console.WriteLine(settings.ValidationError);
+            }
+
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+
+        static void Get(Settings settings)
+        {
+            Get(settings, settings.RemoteDirectory);
+        }
+
+        private static readonly string[] ignoreExtensions = new string[] { ".zip",  };
+
+        private static List<string> fileExtensions = new List<string>();
+
+        static void Get(Settings settings, string remoteDirectoryPath)
+        {
+            foreach (string remoteFilePath in Directory.GetFiles(remoteDirectoryPath))
+            {
+                if (!ignoreExtensions.Contains(Path.GetExtension(remoteFilePath)))
+                {
+                    string localFilePath = Path.Combine(settings.LocalDirectory,remoteFilePath.Substring(remoteDirectoryPath.Length + 1));
+
+                    if (!fileExtensions.Contains(Path.GetExtension(remoteFilePath)))
+                    {
+                        fileExtensions.Add(Path.GetExtension(remoteFilePath));
+                    }
+
+                    //File.Copy(remoteFilePath, localFilePath);
+                }
+            }
+
+            foreach (string subDirectoryPath in Directory.GetDirectories(remoteDirectoryPath))
+            {
+                Get(settings, subDirectoryPath);
+            }
+
+        }
+
+        private static void Clear(string directoryPath)
+        {
+            foreach (string subDirectoryPath in Directory.GetDirectories(directoryPath))
+            {
+                Clear(subDirectoryPath);
+                Directory.Delete(subDirectoryPath);
+            }
+
+            foreach (string filePath in Directory.GetFiles(directoryPath))
+            {
+                File.SetAttributes(filePath, FileAttributes.Normal);
+                File.Delete(filePath);
+            }
+        }
+    }
+}
---
+++ FitnesseUtils/Settings.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace FitnesseUtils
+{
+    public class Settings
+    {
+        public string[] Args { get; set; }
+
+        public Settings(string[] args)
+        {
+            Args = args;
+            ValidationError = "";
+        }
+
+        public bool Load()
+        {
+            return ProcessArgs();
+        }
+
+        private bool ProcessArgs()
+        {
+            bool ret = true;
+
+            string flag = "";
+
+            foreach (string arg in Args)
+            {
+                if (arg.StartsWith("-"))
+                {
+                    flag = arg;
+
+                    switch (flag)
+                    {
+                        case "-c":
+                            Clear = true;
+                            break;
+                        case "-g":
+                            Get = true;
+                            break;
+                    }
+                }
+                else
+                {
+                    switch (flag)
+                    {
+                        case "-r":
+                            RemoteDirectory = arg;
+                            break;
+                        case "-l":
+                            LocalDirectory = arg;
+                            break;
+                        default:
+                            if (arg.StartsWith("-"))
+                            {
+                                ret = false;
+                                AddValidationError("Unrecognised flag '{0}'", arg);
+                            }
+                            break;
+                    }
+                }
+            }
+
+            return ret;
+        }
+
+        public bool Get { get; set; }
+
+        public void AddValidationError(string format, params object[] args)
+        {
+            ValidationError += String.Format(format, args) + Environment.NewLine;
+        }
+
+        public string ValidationError { get; set; }
+
+        public string RemoteDirectory { get; set; }
+
+        public string LocalDirectory { get; set; }
+
+        public bool Clear { get; set; }
+    }
+}
